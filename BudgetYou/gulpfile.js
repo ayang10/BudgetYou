8e58cae0e2b532e58e2f66ce75a512dceb84ec7f
ayang10 <ayang014@gmail.com>
2018-01-07 13:24:01 -0500

    so ugly
    change the routing to just show my login. I just think it's better that
    way. need some more design done to it.

---
+++ BudgetYou/gulpfile.js
@@ -0,0 +1,25 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css');
+
+var config = {
+    bowerDir: './bower_components'
+}
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/sass/*.scss', ['sass']);
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src('./assets/css/style.css')
+        .pipe(minifyCSS({ keepSpecialComments: 1 }))
+        .pipe(gulp.dest('./assets/minify-css'));
+});
+
+gulp.task('default', ['sass', 'watch']);
