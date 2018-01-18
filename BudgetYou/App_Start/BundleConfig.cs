6437876679eb3d7b67be9427349d66b8affe3049
ayang10 <ayang014@gmail.com>
2018-01-18 12:58:53 -0500

    js packages clean up

--- BudgetYou/App_Start/BundleConfig.cs
+++ BudgetYou/App_Start/BundleConfig.cs
@@ -30,53 +30,7 @@ namespace BudgetYou
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/theme/css").Include(
-                     "~/theme/css/bootstrap.css",
-                     "~/theme/css/font-awesome.min.css",
-                     "~/theme/css/less-style.css",
-                     "~/theme/css/style.css"));
-
-            bundles.Add(new StyleBundle("~/theme/js").Include(
-                      "~/theme/js/jquery.js",
-                      "~/theme/js/bootstrap.min.js",
-                      "~/theme/js/respond.min.js",
-                      "~/theme/js/html5shiv.js",
-                      "~/theme/js/jquery-ui.min.js",
-                      "~/theme/js/bootstrap-datetimepicker.min.js",
-                      "~/theme/js/wysihtml5-0.3.0.js",
-    "~/theme/js/prettify.js", 
-   "~/theme/js/bootstrap-wysihtml5.min.js", 
-   
-         "~/theme/js/jquery.flot.min.js", 
-           "~/theme/js/jquery.flot.stack.min.js" ,
-            "~/theme/js/jquery.flot.pie.min.js",
-              "~/theme/js/jquery.flot.resize.min.js",
-          
-           
-            "~/theme/js/jquery.validate.js" ,
-         
-            "~/theme/js/jquery.steps.min.js",
-          
-            "~/theme/js/jquery.knob.js", 
-              
-           "~/theme/js/jquery.sparklines.js",
-               
-             "~/theme/js/jquery.slimscroll.min.js", 
-               
-             "~/theme/js/jquery.dataTables.min.js", 
-               
-            "~/theme/js/jquery.prettyPhoto.js", 
-              
-           "~/theme/js/jquery.rateit.min.js",
-            "~/theme/js/moment.min.js" ,
-                     "~/theme/js/fullcalendar.min.js",
-                   
-                 "~/theme/js/jquery.gritter.min.js",
-      "~/theme/js/custom.notification.js",
-      "~/theme/js/custom.js" 
-                      ));
-
+            
         }
     }
 }
